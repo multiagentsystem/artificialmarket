c67289dc43b3dea9a8f77bb799914e06d5ed821c
naritomi <naritomi@192.168.100.23>
2019-02-08 23:11:39 +0900

    修正した

---
+++ src/world/learning/generic/generic.cpp
@@ -0,0 +1,18 @@
+/*
+ * generic.cpp
+ *
+ *  Created on: 2019/02/08
+ *      Author: yusuke
+ */
+
+#include <learning/generic/generic.h>
+
+generic::generic() {
+	// TODO 自動生成されたコンストラクター・スタブ
+
+}
+
+generic::~generic() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/world/learning/generic/generic.h
@@ -0,0 +1,17 @@
+/*
+ * generic.h
+ *
+ *  Created on: 2019/02/08
+ *      Author: yusuke
+ */
+
+#ifndef GENERIC_H_
+#define GENERIC_H_
+
+class generic {
+public:
+	generic();
+	virtual ~generic();
+};
+
+#endif /* GENERIC_H_ */
